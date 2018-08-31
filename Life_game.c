f8307264d501c432ba2c9745006a268d474433b1
Eintha <eintha8@gmail.com>
2018-08-31 20:19:09 -0300

    puto el que lee

---
+++ Life_game.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define SIZE 8
+void lgame (void);
+
+
+int main (void)
+{
+    int input;
+    char primordial [SIZE] [SIZE];
+    char universe [SIZE] [SIZE];
+    char future [SIZE] [SIZE];
+    universe = primordial;
+
+    input=getchar();
+
+    if (input == 'N')
+    {
+        lgame ();
+    }
+    else (input == 'R')
+    {
+        universe = primordial;
+    }
+    input=getchar();
+}
