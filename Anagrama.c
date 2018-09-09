8ae83146cff6887dae4704bda4c35f0fd08b7f1d
Pdutriez <pdutriez@itba.edu.ar>
2018-09-09 20:01:09 -0300

    jioji

--- Anagrama.c
+++ Anagrama.c
@@ -2,7 +2,7 @@
 #define L_MAX 46      //Maximo de letras por palabra
 #define TERM 0      //Terminador
 void comparer (char arr1 [L_MAX], char arr2 [L_MAX]); //Funcion que compara ambas palabras
-void arr_init (char input, char arr1 [L_MAX],  char arr2 [L_MAX]); //Funcion que crea los arreglos de las palabras
+int arr_init (char input, char arr1 [L_MAX],  char arr2 [L_MAX]); //Funcion que crea los arreglos de las palabras
 
 
 int main (void)
@@ -10,12 +10,13 @@ int main (void)
     char fuente [L_MAX]; //Arreglo para la segunda palabra
     char copia [L_MAX]; //Arreglo para la primer palabra
     char input;
-    printf("fuente = %p copia = %p", &fuente, &copia);
     while((input=getchar()) != EOF)
     {
-    arr_init(input,copia,fuente);
-    printf("Entre las palabras %s y %s \n",copia, fuente);
-    comparer (fuente,copia);
+    if(arr_init(input,copia,fuente))
+    {
+      printf("Para formar la palabra %s, las letras de %s ",fuente,copia);
+      comparer (fuente,copia);
+    }
     }
   return 0;
 }
@@ -25,12 +26,12 @@ int main (void)
 
 //Funciones
 
-void arr_init (char input,char arr1 [L_MAX], char arr2 [L_MAX]) //Funcion que crea los arreglos de las palabras
+int arr_init (char input,char arr1 [L_MAX], char arr2 [L_MAX]) //Funcion que crea los arreglos de las palabras
 {
     char *pointer;
     char mycopy = input;
+    int error = 1; //Variable para indicar error
     pointer = arr1;
-    printf("pointer = %p", pointer);
     for( ;mycopy != '\n';++pointer,(mycopy=getchar()))
     {
       if((mycopy <= 'z') && (mycopy >= 'a'))
@@ -41,13 +42,20 @@ void arr_init (char input,char arr1 [L_MAX], char arr2 [L_MAX]) //Funcion que cr
       else if(mycopy == '/')
       {
           *pointer = TERM; //Ponemos un terminador
-          pointer = arr2; //Cambiamos de arreglo
-          printf("pointer = %p", pointer);
+          pointer = arr2-1; //Cambiamos de arreglo
       }
       else if((mycopy <= 'Z') && (mycopy >= 'A'))
-      *pointer=mycopy; //Guardamos el caracter
+      {
+        *pointer=mycopy; //Guardamos el caracter
+      }
+      else
+      {
+        printf("el caracter '%c' no es una letra. \n \n", mycopy);
+        error = 0;
+      }
     }
     *pointer=TERM;
+    return error;
 }
 
 void comparer (char arr1 [L_MAX], char arr2 [L_MAX])   //Chequea si es posible escribir la segunda palabra con las letraso de la primera
@@ -73,10 +81,10 @@ void comparer (char arr1 [L_MAX], char arr2 [L_MAX])   //Chequea si es posible e
         }
         if (finder != 1)                          //En caso de que no sea uno implica que no se encontro la letra
         {
-            printf("Error, la letra %c no fue encontrada.\n",*word2);
+            printf("son insuficientes, la letra %c no fue encontrada.\n \n",*word2);
             *(word2+1)=TERM;          //Le indicamos al for principa que no es necesario seguir analizando
         }
     }
     if(finder == 1)                             //En caso de que se haya encontrado, finder es igual a 1
-    printf ("Las letras son suficientes para escribir la palabra deseada.\n");
+    printf ("son suficientes para poder escribirla.\n \n");
 }
