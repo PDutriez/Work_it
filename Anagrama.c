4fb5453a6b962f6504c971e9e656894845b087fa
Pdutriez <pdutriez@itba.edu.ar>
2018-09-09 18:50:59 -0300

    weon

--- Anagrama.c
+++ Anagrama.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #define L_MAX 46      //Maximo de letras por palabra
-#define TERM '\0'      //Terminador
-void comparer (char arr1 [L_MAX], char arr2 [L_MAX]);
-void arr_init (void);
+#define TERM 0      //Terminador
+void comparer (char arr1 [L_MAX], char arr2 [L_MAX]); //Funcion que compara ambas palabras
+void arr_init (char input, char arr1 [L_MAX],  char arr2 [L_MAX]); //Funcion que crea los arreglos de las palabras
 
 
 int main (void)
 {
     char fuente [L_MAX]; //Arreglo para la segunda palabra
     char copia [L_MAX]; //Arreglo para la primer palabra
-    char *pointer;
-    pointer = copia;
-    for( ;(input=getchar()) != EOF; ++pointer)
+    char input;
+    printf("fuente = %p copia = %p", &fuente, &copia);
+    while((input=getchar()) != EOF)
     {
-    arr_init(input);
+    arr_init(input,copia,fuente);
     printf("Entre las palabras %s y %s \n",copia, fuente);
-    comparer (char fuente [L_MAX], char copia [L_MAX]);
+    comparer (fuente,copia);
     }
-
+  return 0;
 }
 
 
@@ -25,33 +25,39 @@ int main (void)
 
 //Funciones
 
-void arr_init (char input)
+void arr_init (char input,char arr1 [L_MAX], char arr2 [L_MAX]) //Funcion que crea los arreglos de las palabras
 {
     char *pointer;
-    pointer = copia;
-    for( ;input != '\n';++pointer,(input=getchar()))
+    char mycopy = input;
+    pointer = arr1;
+    printf("pointer = %p", pointer);
+    for( ;mycopy != '\n';++pointer,(mycopy=getchar()))
     {
-      if((input <= 'z') || (input >= 'a'))
-      input-= 'a' - 'A'; //Convertimos las minusculas en mayusculas para asi tener el mismo tipo de letras
-      *pointer= input; //Guardamos el caracter
-      else if(input == '/')
+      if((mycopy <= 'z') && (mycopy >= 'a'))
+      {
+        mycopy-= 'a' - 'A'; //Convertimos las minusculas en mayusculas para asi tener el mismo tipo de letras
+        *pointer= mycopy; //Guardamos el caracter
+      }
+      else if(mycopy == '/')
       {
           *pointer = TERM; //Ponemos un terminador
-          pointer = fuente; //Cambiamos de arreglo
+          pointer = arr2; //Cambiamos de arreglo
+          printf("pointer = %p", pointer);
       }
-      else if((input <= 'z') || (input >= 'a'))
-      *pointer=input; //Guardamos el caracter
+      else if((mycopy <= 'Z') && (mycopy >= 'A'))
+      *pointer=mycopy; //Guardamos el caracter
     }
+    *pointer=TERM;
 }
 
-void comparer (char fuente [L_MAX], char copia [L_MAX])   //Chequea si es posible escribir la segunda palabra con las letraso de la primera
+void comparer (char arr1 [L_MAX], char arr2 [L_MAX])   //Chequea si es posible escribir la segunda palabra con las letraso de la primera
 {
     char *word2, *word1;                                 //Pointer a la segunda palabra
     int finder, counter;
-    word2  = fuente;
+    word2  = arr1;
     for ( ;*word2 != TERM; word2++)
     {
-        word1 = copia;                                  //Pointer a la primera palabra
+        word1 = arr2;                                  //Pointer a la primera palabra
         for (finder=0, counter=0; counter<L_MAX; counter++, word1++)
         {
             if (*word2 == *word1)
