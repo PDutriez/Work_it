775c5b0fd635c2c6ff7e182d34787eb521d466c9
Pdutriez <pdutriez@itba.edu.ar>
2018-09-09 18:14:56 -0300

    recatate rulo verde

--- Anagrama.c
+++ Anagrama.c
@@ -7,11 +7,17 @@ void arr_init (void);
 
 int main (void)
 {
-    char fuente [L_MAX];
-    char copia [L_MAX];
-    arr_init;
+    char fuente [L_MAX]; //Arreglo para la segunda palabra
+    char copia [L_MAX]; //Arreglo para la primer palabra
+    char *pointer;
+    pointer = copia;
+    for( ;(input=getchar()) != EOF; ++pointer)
+    {
+    arr_init(input);
+    printf("Entre las palabras %s y %s \n",copia, fuente);
     comparer (char fuente [L_MAX], char copia [L_MAX]);
-    return 0;
+    }
+
 }
 
 
@@ -19,39 +25,33 @@ int main (void)
 
 //Funciones
 
-void arr_init (void)                 //Recibe por teclado las palabras y las guarda en arreglos
+void arr_init (char input)
 {
-    char letter = getchar ();       //Variable para recibir el input del teclado
-    char *pointer = copia;         //Puntero para la copia de los arreglos
-    while (letter != '/' && (letter >= 'a') && (letter <='z'))
-    {                               //Copia la primera palabra hasta percibir el '/'
-        *pointer = letter;          //Y paulatinamente la guarda en un arreglo
-        letter = getchar ()
-        pointer++;
+    char *pointer;
+    pointer = copia;
+    for( ;input != '\n';++pointer,(input=getchar()))
+    {
+      if((input <= 'z') || (input >= 'a'))
+      input-= 'a' - 'A'; //Convertimos las minusculas en mayusculas para asi tener el mismo tipo de letras
+      *pointer= input; //Guardamos el caracter
+      else if(input == '/')
+      {
+          *pointer = TERM; //Ponemos un terminador
+          pointer = fuente; //Cambiamos de arreglo
+      }
+      else if((input <= 'z') || (input >= 'a'))
+      *pointer=input; //Guardamos el caracter
     }
-
-    pointer = fuente;              //Apunta el puntero al nuevo arreglo
-    letter = getchar ();
-    while (letter != '\n' && (letter >= 'a') && (letter <='z'))
-    {                             //Copia la segunda palabra hasta llegar al '\n'
-        *pointer = letter;        //Y se guarda en el segundo arreglo
-        letter = getchar ()
-        pointer++;
-    }
-    return 0;
 }
 
-
-
-void comparer (char fuente [L_MAX], char copia [L_MAX])   //Chequea si es posible escribir
-{                                                         //la segunda palabra con las letras
-    char *word2 = fuente;                                 //de la primera
-    for ( ; *word2 != TERM; word2++)
+void comparer (char fuente [L_MAX], char copia [L_MAX])   //Chequea si es posible escribir la segunda palabra con las letraso de la primera
+{
+    char *word2, *word1;                                 //Pointer a la segunda palabra
+    int finder, counter;
+    word2  = fuente;
+    for ( ;*word2 != TERM; word2++)
     {
-        char *word1 = copia;
-
-        int finder;
-        int counter;
+        word1 = copia;                                  //Pointer a la primera palabra
         for (finder=0, counter=0; counter<L_MAX; counter++, word1++)
         {
             if (*word2 == *word1)
@@ -60,11 +60,17 @@ void comparer (char fuente [L_MAX], char copia [L_MAX])   //Chequea si es posibl
                 finder++;
                 counter = L_MAX-1;
             }
+            if(*word1== TERM)
+            {
+
+            }
         }
-        if (finder != 1)
+        if (finder != 1)                          //En caso de que no sea uno implica que no se encontro la letra
         {
             printf("Error, la letra %c no fue encontrada.\n",*word2);
+            *(word2+1)=TERM;          //Le indicamos al for principa que no es necesario seguir analizando
         }
     }
-    printf ("Las letras son suficientes para escribir la palabra deseada.");
+    if(finder == 1)                             //En caso de que se haya encontrado, finder es igual a 1
+    printf ("Las letras son suficientes para escribir la palabra deseada.\n");
 }
