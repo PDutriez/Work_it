033ad30bf43915e9f2105c4fd0c9d13eb3ceef01
Eintha <eintha8@gmail.com>
2018-09-29 10:26:47 -0300

    Modulo principal

---
+++ The future is now/LGMain.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "PrintNChange.h"
+#include "Creation.h"
+
+int main (void)
+{
+    int input, num, talive=1;
+    char universe [MAX][MAX];
+    char future [MAX] [MAX];
+    printf("Este es el juego de la vida, usted tiene la opcion de elegir el tamaño \n");
+    printf("de la matriz entre 1 y 100, indique el tamaño que desea usar:...\n");
+    while((input=getchar()) != EOF)
+    {
+      if(input<='9' && input>='0')
+      {
+          input-='0';
+          num=(num*10)+input; //Se actualiza el tamaño de la matriz
+      }
+      else if (input=='\n')
+      {
+          if (num<=100 && num>=1)
+          {
+              create(num);
+          }
+          else if
+          {
+               printf("\nVuelva a ingresar los datos");
+          }
+          putchar('\n');
+          num=0; //Si se cargaron bien lo datos no pasa nada pero en caso contrario se necesita resetear este valor
+      }
+      else if (input == 'S' || input == 's') //Se inicia el juego
+      {
+          printf("Si quiere parar el juego aprete 'CTRL + C'\n");
+          time_clean();
+          while(talive!=0)
+          {
+              print_mundo(universe);  //Se imprime el mundo actual
+              talive=deadoralive(universe,future);  //Se lo analiza y ademas verifica que no este vacio
+              copypaste(future, universe); //Se lo copia en la otra matriz para imprimirlo al empezar el while
+              time_clean();
+          }
+          printf("Parece que el juego ha terminado....\n");
+          inicio=0, tope=0, talive=1; //Se cargan estos valores para poder reiniciar el juego en caso de terminarse
+      }
+    }
+
+}
