a27f31f0a396da70dfc6f920315d4fa5c9976035
Eintha <eintha8@gmail.com>
2018-09-29 10:28:01 -0300

    Modulo impresion y actualizacion

---
+++ The future is now/PrintNChange.c
@@ -0,0 +1,39 @@
+#include "PrintNChange.h"
+#include "Creation.h"
+#include <stdio.h>
+
+void print_mundo(char arr[MAX][MAX])                            //Esta funcion imprime la matriz en pantalla
+{
+    int countfil, countcol;
+    for (countfil=inicio; countfil<tope; ++countfil)
+    {
+                                                                //Con esto señalamos el segundo elemento de la segunda fila
+        for (countcol=inicio; countcol<tope; ++countcol)
+        {
+            putchar('|');                                       //Separacion entre columnas
+            printf("%c ",arr[countfil][countcol]);
+        }
+        putchar('|');                                           //se ponen guiones para separar el borde
+        putchar('\n');                                          //Se pone ENTER para que inicie la nueva linea
+    }
+}
+void copypaste(char copy[MAX][MAX], char paste[MAX][MAX])       //Copia una matriz en otra
+{
+    int countfil, countcol;
+    for (countfil=inicio; countfil<tope; ++countfil)            //Menos a la cantidad de columnas totales
+    {
+        for (countcol=0; countcol<tope; ++countcol)             //Menor a la cantidad de filas totales
+        {
+            paste[countfil][countcol]=copy[countfil][countcol]; //Igualamos sus valores
+        }
+    }
+}
+
+void time_clean(void)                                           //Esta funcion se encarga de hacer tiempo y limpiar la pantalla para hacer que se vea bien la nueva generacion
+{
+    long time;
+    for(time=0;time<300000000;++time);                          //El tiempo fijado fue elegido a ojo y gusto del programador
+    for(time=0;time<inicio;++time)                              //Se limia la pantalla para que aparezca ordenado
+    printf("\n");
+
+}
---
+++ The future is now/PrintNChange.h
@@ -0,0 +1,6 @@
+/*******************************************************************************
+ * PROTOTIPOS DE FUNCIONES
+ ******************************************************************************/
+ void copypaste(char copy[MAX][MAX],char paste[MAX][MAX]);    //Copia una matriz en otra
+ void print_mundo(char arr[MAX][MAX]);                        //Esta funcion imprime la matriz en pantalla
+ void time_clean(void);                                       //Esta funcion se encarga de hacer tiempo y limpiar la pantalla para hacer que se vea bien la nueva generacion
