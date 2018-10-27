e76bc27645c2e2d9c8269bd13149b8c93a59ef50
PDutriez <42210994+PDutriez@users.noreply.github.com>
2018-10-27 19:24:00 -0300

    VERSION BUENA DE PALINDROMO (FUNCIONA PERFECTO)

---
+++ TP11/palindromo.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include <string.h>
+int palindromo(char* arr,unsigned int length);
+#define outsmall(x)    (x<'a' || x>'z')
+#define outcap(x)    (x<'A' || x>'Z')
+
+int main() 
+{
+    char* chk="ReConocEr";
+   if(palindromo(chk,strlen(chk)))
+    printf("Es palindromo");
+   else
+    printf("No es palindromo");
+}
+
+int palindromo(char* arr,unsigned int length)
+{
+    if(&arr[0]>=&arr[length-1]) //Verificamos que no se crucen las posiciones
+            return 1;  
+    else if(outcap(arr[0])&&outsmall(arr[0])) //Verificamos que sea letra
+            return(palindromo(++arr,length));
+    else if(outcap(arr[length-1])&&outsmall(arr[length-1])) //Verificamos que sea letra
+            return(palindromo(arr,length-2));
+    else if(arr[0]==arr[length-1]) //Analizamos si las letras son iguales
+            return(palindromo(++arr,length-2));
+    else if((arr[0]-('a'-'A'))==arr[length-1]) //Analizamos si la primera es minuscula y la segunda mayuscula
+            return(palindromo(++arr,length-2));
+    else if(arr[0]==(arr[length-1]-('a'-'A'))) //Analizamos si la primera es mayuscula y la segunda minuscula
+            return(palindromo(++arr,length-2));
+    else //Si no ha sido ninguno de los casos anteriores, implica que no es palindromo
+            return 0;
+}
